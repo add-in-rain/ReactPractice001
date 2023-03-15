ba7b1f0c9ac0fa10dce7fe288f9d7c70b1abb793
add-in-rain <hih8752@naver.com>
2023-03-15 16:32:32 +0900

    연습

---
+++ RP/rp/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Header from './components/Header/Header'
+// import Main from './components/Main/Main'
+// import Cards from './components/Main/Cards'
+import Blog from './components/Blog/Blog'
+
+function App() {
+  return (
+    <div className="App">
+      <Blog/>
+      {/* <Header/>
+      <Main/>
+      <Cards/> */}
+    </div>
+  );
+}
+
+export default App;
