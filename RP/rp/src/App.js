d5d62e5def441a35b1b9fcbc9644923315f3e965
add-in-rain <hih8752@naver.com>
2023-03-16 17:30:17 +0900

    연습

--- RP/rp/src/App.js
+++ RP/rp/src/App.js
@@ -1,16 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Header from './components/Header/Header'
-// import Main from './components/Main/Main'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 // import Cards from './components/Main/Cards'
 import Blog from './components/Blog/Blog'
+import { Routes, Route, Link } from "react-router-dom";
+import Sub1 from './components/Sub1'
+import Menu2 from './components/Menu2';
 
 function App() {
   return (
-    <div className="App">
-      <Blog/>
-      {/* <Header/>
-      <Main/>
+    <div className="App">    
+      {/* <Blog/> */}
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/menu1' element={<h1>menu1 페이지</h1>}/>
+        <Route path='/menu2' element={<Menu2/>}/>
+      </Routes>
+
+      {/* <Main/>
       <Cards/> */}
     </div>
   );
--- RP/rp/src/components/Blog/Blog.jsx
+++ RP/rp/src/components/Blog/Blog.jsx
@@ -9,6 +9,11 @@ const Blog = () => {
     "Title3",
   ])
 
+  // 모달창을 띄우기 위한 상태값
+  let [modal, setmodal] = useState(false);
+  // 모달창에 표시될 제목 글의 인덱스
+  let [modalTitle, setmodalTitle] = useState(0);
+
   const name = ["이름1", "이름2", "이름3"]
 
   return (
@@ -20,7 +25,14 @@ const Blog = () => {
       {
         title.map((item, i)=>{
           return (
-            <div className='list'>
+            <div className='list' onClick={()=>{
+              if(modal && modalTitle === i) {
+                setmodal(false)
+              } else {
+                setmodal(true);
+                setmodalTitle(i)
+              }
+            }}>
               <h4>{item}</h4>
               <p>안녕하세요 저는 {name[i]} 입니다.</p>
               <button>글삭제</button>
@@ -28,16 +40,18 @@ const Blog = () => {
           )
         })
       }
+
+      {modal && <Modal modalTitle={modalTitle} title={title} closeModal={()=> setmodal(false)} />}
     </div>
   )
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className='modal'>
-      <h4>Title</h4>
+      <h4>{props.title[props.modalTitle]}</h4>
       <p>Content</p>
-      <button>닫기</button>
+      <button onClick={props.closeModal}>닫기</button>
     </div>
   )
 }
